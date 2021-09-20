caa125a9ac1f04d7e5df0f3ca5a493a8c363b2b7
Justin Naismith <justin.n@live.com.au>
2021-09-20 21:08:56 +1000

    refactored keypress

--- src/lib/Keypress.svelte
+++ src/lib/Keypress.svelte
@@ -8,14 +8,13 @@
   export let start: () => void;
   export let reset: () => void;
 
-  function handleKeypress(event: { key: string }) {
+  function handleKeypress(event: { key: string; preventDefault: () => void }) {
     key = event.key;
     const regex = /^[\w\W]$/gm; // true if any single char - no words or modifiers
     const isChar = regex.test(key);
 
     if (!gameRunning && isChar) {
       // if the game is not running, start the game if character is typed
-      gameRunning = true;
       start();
     } else if (gameRunning && key === 'Escape') {
       // if the game is running and escape key is pressed, reset the game
@@ -23,16 +22,38 @@
       reset();
     }
 
-    if (key === 'Backspace') {
-      currentChars.pop();
-      typedChars.push(key);
-    } else if (isChar) {
-      currentChars.push(key);
-      typedChars.push(key);
+    switch (key) {
+      case 'Tab':
+        handleTab(event);
+        break;
+      case 'Backspace':
+        handleBackspace(key);
+        break;
+      default:
+        handleChar(key, isChar);
+        break;
     }
     currentChars = currentChars;
     // push all typed characters to calc the accuracy
     typedChars = typedChars;
+  }
+
+  function handleTab(event: { key?: string; preventDefault: any }) {
+    // I could use the button ref but that would require some prop drilling I want to avoid
+    event.preventDefault();
+    document.getElementById('restart-button').focus();
+  }
+
+  function handleBackspace(key: string) {
+    currentChars.pop();
+    typedChars.push(key);
+  }
+
+  function handleChar(key: string, isChar: boolean) {
+    if (isChar) {
+      currentChars.push(key);
+      typedChars.push(key);
+    }
   }
 </script>
 
