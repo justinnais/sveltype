b27f2e3dc63c422620bdc8907a28e5d4162fcdf1
Justin Naismith <justin@nais.dev>
2022-09-12 23:13:18 +1000

    refactor: cleanup exports

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,6 @@
+export { generateWords } from './generate';
+export { wordsToChars } from './wordsToChars';
+export { calculateAccuracy } from './calculateAccuracy';
+export { getCurrentTime } from './timeLogic';
+export { calculateWPM } from './calculateWPM';
+export { getCurrentWord } from './getCurrentWord';
