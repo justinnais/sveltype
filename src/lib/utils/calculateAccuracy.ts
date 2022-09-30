877d88bbfc0786cb6805ba769df59b4a4d248790
Justin Naismith <justin@nais.dev>
2022-09-30 17:22:12 +1000

    test: added accuracy tests

--- src/lib/utils/calculateAccuracy.ts
+++ src/lib/utils/calculateAccuracy.ts
@@ -1,7 +1,12 @@
 export function calculateAccuracy(currentChars: string[], errors: number): number {
+  if (errors < 0 || isNaN(errors)) {
+    throw new Error('errors must be a positive number');
+  }
+
   const charLength = currentChars.length;
   const accuracy = parseInt((((charLength - errors) / charLength) * 100).toFixed(0));
-  if (accuracy < 0) {
+
+  if (Number.isNaN(accuracy) || accuracy < 0) {
     return 0;
   }
 
